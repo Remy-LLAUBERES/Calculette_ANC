c0fa5f2805dbd4b9eb2525fb6b13b7f3e5a782b5
Remy-LLAUBERES <remy.llauberes@gmail.com>
2025-06-20 11:37:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = "cache-anc-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./Logo.ico",
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('anc-cache').then(function(cache) {
+      return cache.addAll([
+        "./",
+	    "./index.html",
+	    "./manifest.json",
+	    "./Logo.ico",
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
+.catch(err => console.error("Erreur de mise en cache", err));
